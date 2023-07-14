50523e319d40d6803e65ca5ca6829ce3e3cca0b4
vcokltfre <vcokltfre@gmail.com>
2023-07-14 12:50:46 +0100

    chore: minor anti-abuse checks

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,6 @@
 from asyncio import run
 from os import environ
+from time import time
 
 from disnake import CommandInteraction, Message
 from disnake.ext.commands import InteractionBot
@@ -12,8 +13,18 @@ load_dotenv()
 async def main() -> None:
     bot = InteractionBot()
 
+    cache: set[int] = set()
+
     @bot.message_command(name="degallery")  # type: ignore
     async def degallery(itr: CommandInteraction, message: Message):  # type: ignore
+        if message.id in cache:
+            return
+
+        if message.created_at.timestamp() < (time() - 600):
+            return
+
+        cache.add(message.id)
+
         urls: list[str] = []
 
         for attachment in message.attachments:
